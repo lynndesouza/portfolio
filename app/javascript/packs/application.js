b4d1d5832338896b0669c16c29b3c028690da3fd
Lynn De Souza <lynn.desouza@alumni.uct.ac.za>
2019-01-21 15:15:04 +0000

    navbar updates

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,11 @@
 import "bootstrap";
 import "agency.min.js";
 import "timeline.js";
-import "fadein.js";
-import "fadeout.js";
 import "banner.js";
 import "typed.js";
-
 import "hi.js";
 import "services.js";
 
-import "fade.js";
 
 
 
